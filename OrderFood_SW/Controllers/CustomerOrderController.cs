dee84d7c9588a764069ff1391a0f09696c9b8146
NguyenQuangMinhTuan <144577288+NguyenQuangMinhTuan@users.noreply.github.com>
2025-08-05 17:16:09 +0700

    fix some bugs, handle session Customer Order

--- OrderFood_SW/Controllers/CustomerOrderController.cs
+++ OrderFood_SW/Controllers/CustomerOrderController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using OrderFood_SW.Helper;
+using OrderFood_SW.Models;
 using OrderFood_SW.ViewModels;
 
 namespace OrderFood_SW.Controllers
@@ -28,6 +30,7 @@ namespace OrderFood_SW.Controllers
             ViewBag.TableId = HttpContext.Session.GetInt32("CurrentTableId") ?? 0;
 
             var query = _db.Dishes.AsQueryable();
+            var queryCategories = _db.Categories.AsQueryable();
 
             if (!string.IsNullOrEmpty(searchKeyword))
             {
@@ -35,8 +38,8 @@ namespace OrderFood_SW.Controllers
             }
 
 
-            var dishes = query.OrderBy(d => d.DishName)
-                              .ToList();
+            var dishes = query.OrderBy(d => d.DishName).ToList();
+            var categories = queryCategories.OrderBy(c => c.CategoryName).ToList();
 
             var cart = HttpContext.Session.GetObject<List<OrderCartItem>>("Cart") ?? new List<OrderCartItem>();
 
@@ -44,10 +47,111 @@ namespace OrderFood_SW.Controllers
             {
                 SearchKeyword = searchKeyword,
                 FoundDishes = dishes,
+                DishCategories = categories,
                 CartItems = cart
             };
 
             return View(model);
+        }
+
+        // Customer cart management **** **** **** ****
+        [HttpPost]
+        public IActionResult AddCart(int dishId, int Quantity)
+        {
+            var dish = _db.Dishes.Find(dishId);
+            var cart = HttpContext.Session.GetObject<List<OrderCartItem>>("Cart") ?? new List<OrderCartItem>();
+
+            var existing = cart.FirstOrDefault(x => x.DishId == dishId);
+            if (existing != null)
+                existing.Quantity += Quantity;  // Add the selected quantity
+            else
+                cart.Add(new OrderCartItem
+                {
+                    DishId = dish.DishId,
+                    ImageUrl = dish.ImageUrl,
+                    DishName = dish.DishName,
+                    Price = dish.DishPrice,
+                    Quantity = Quantity
+                });
+
+            HttpContext.Session.SetObject("Cart", cart);
+            return RedirectToAction("CreateOrder");
+        }
+
+        public IActionResult GetCart()
+        {
+            var cart = HttpContext.Session.GetObject<List<OrderCartItem>>("Cart") ?? new List<OrderCartItem>();
+            return PartialView("_CartPartial", cart);
+        }
+
+        [HttpPost]
+        public IActionResult RemoveFromCart(int id)
+        {
+            var cart = HttpContext.Session.GetObject<List<OrderCartItem>>("Cart") ?? new List<OrderCartItem>();
+
+            var itemToRemove = cart.FirstOrDefault(x => x.DishId == id);
+            if (itemToRemove != null)
+            {
+                cart.Remove(itemToRemove);
+                HttpContext.Session.SetObject("Cart", cart);
+            }
+
+            return Json(new { success = true, count = cart.Count });
+        }
+
+        [HttpPost]
+        public IActionResult RemoveAllCart()
+        {
+            HttpContext.Session.Remove("Cart");
+            return Json(new { success = true });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> OrderInitAsync(int tableId)
+        {
+            var cart = HttpContext.Session.GetObject<List<OrderCartItem>>("Cart") ?? new List<OrderCartItem>();
+
+            if (!cart.Any())
+            {
+                TempData["Error"] = "Giỏ hàng trống!";
+                return RedirectToAction("Create", new { tableId });
+            }
+
+            var order = new Order
+            {
+                TableId = tableId,
+                OrderTime = DateTime.Now,
+                OrderStatus = 1,
+                TotalAmount = cart.Sum(x => x.Price * x.Quantity),
+                note = "n/a"
+            };
+
+            _db.Orders.Add(order);
+            _db.SaveChanges();
+
+            foreach (var item in cart)
+            {
+                var orderDetail = new OrderDetail
+                {
+                    OrderId = order.OrderId,
+                    DishId = item.DishId,
+                    Quantity = item.Quantity
+                };
+
+                _db.OrderDetails.Add(orderDetail);
+            }
+
+            var table = await _db.Tables.FirstOrDefaultAsync(t => t.TableId == order.TableId);
+            if (table != null)
+            {
+                table.Status = "Occupied";
+            }
+
+
+            _db.SaveChanges();
+            HttpContext.Session.Remove("Cart");
+
+            return RedirectToAction("Detail", new { orderId = order.OrderId });
         }
     }
 }
--- OrderFood_SW/ViewModels/OrderCartItem.cs
+++ OrderFood_SW/ViewModels/OrderCartItem.cs
@@ -3,6 +3,7 @@
     public class OrderCartItem
     {
         public int DishId { get; set; }
+        public string ImageUrl { get; set; }
         public string DishName { get; set; }
         public decimal Price { get; set; }
         public int Quantity { get; set; }
