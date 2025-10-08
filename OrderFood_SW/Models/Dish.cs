cd16f6e8b65075765a72cb06728770db1fe9873a
NguyenQuangMinhTuan <144577288+NguyenQuangMinhTuan@users.noreply.github.com>
2025-10-08 11:16:58 +0700

    backup

--- OrderFood_SW/Models/Dish.cs
+++ OrderFood_SW/Models/Dish.cs
@@ -13,8 +13,8 @@ namespace OrderFood_SW.Models
         public string? ImageUrl { get; set; }
         public int CategoryId { get; set; }
         public bool IsAvailable { get; set; }
-        [Range(0, double.MaxValue, ErrorMessage = "Tax amount must be a positive number")]
-        public decimal? TaxAmount { get; set; }
+        [Range(0, double.MaxValue, ErrorMessage = "Tax rate must be a positive number")]
+        public decimal? TaxRate { get; set; } = 0.1m;
 
         [NotMapped]
         public IFormFile? ImageFile { get; set; }
